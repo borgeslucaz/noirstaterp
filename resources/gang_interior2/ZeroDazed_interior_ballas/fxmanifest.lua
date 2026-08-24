6cbe9bf2dbd5f4f219ffe7ab049387349eab7047
Lucas Borges <lucasmfborges@gmail.com>
2026-08-24 00:02:06 -0300

    ballas

--- resources/gang_interior2/ZeroDazed_interior_ballas/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-author 'ZeroRespect'
-description 'ZeroDazed Interior Ballas  - Free Map'
-version '1.0.0'
-this_is_a_map 'yes'
--- resources/gang_interior2/ZeroDazed_interior_families/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-author 'ZeroRespect'
-description 'ZeroDazed Interior Families  - Free Map'
-version '1.0.0'
-this_is_a_map 'yes'
