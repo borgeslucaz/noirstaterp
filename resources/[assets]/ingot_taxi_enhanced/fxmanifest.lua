b6ccf823fa900fa6e3a61e3b45e6331d7baf1225
Lucas Borges <lucasmfborges@gmail.com>
2026-09-05 10:47:53 -0300

    adding ingot taxi

---
+++ resources/[assets]/ingot_taxi_enhanced/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Yuri'
+description 'Ingot_Taxi'
+
+files {
+
+	'data/*.meta'
+}
+ 
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+
